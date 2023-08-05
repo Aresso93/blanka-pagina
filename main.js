98432162fb30bf76466e97d011fe4171a904ffee
Aresso93 <ares.fiumicelli@gmail.com>
2023-08-05 19:05:17 +0200

    /

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+const localStorageKey = 'testo';
+
+function updateCounters(){
+    const textarea = document.getElementById('textarea')
+    const counterDiv = document.getElementById('counter-div')
+    const wordsCount = countWords(textarea.value)
+    const charactersCount = countCharacters(textarea.value)
+    counterDiv.textContent = 'Hai scritto ' + wordsCount + ' parole e ' + charactersCount + ' caratteri'
+    textarea.style.resize = "none";
+}
+
+textarea.addEventListener('input', updateCounters)
+
 function switchThemes(){
 
     console.log('TATSUMAKI SENPUKYAKU');
@@ -11,12 +24,14 @@ function switchThemes(){
     }
 
 }
-
-textarea.style.resize = "none";
+// const textarea = document.getElementById('textarea')
+// const counterDiv = document.getElementById('counter-div')
+// counterDiv.appendChild(document.createTextNode('Hai scritto ' + countWords(textarea.value) + ' parole e ' + countCharacters(textarea.value) + ' caratteri'))
+// textarea.style.resize = "none";
 
 function saveLocally(){
     const textarea = document.getElementById('textarea');
-    const localStorageKey = 'testo';
+    
     textarea.addEventListener('input', function (event) {
     const text = event.target.value;
     localStorage.setItem(localStorageKey, text)    
@@ -32,10 +47,30 @@ function loadLocally(){
         if (savedText){
             textarea.value = savedText
         }
-    })
+    });
 
 }
+
+function countCharacters(text){
+    const textWithoutSpaces = text.replace(/\s+/g, '');
+    return textWithoutSpaces.length;
+}
+
+function countWords(text){
+    const trimmedText = text.trim();
+
+    if(trimmedText === ''){
+        return 0
+    }
+
+    const wordsArray = trimmedText.split(/\s+/);
+    return wordsArray.length;
+}
+
+
 
 saveLocally()
 
 loadLocally()
+
+updateCounters()
